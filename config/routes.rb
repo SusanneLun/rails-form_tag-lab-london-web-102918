8774e308e9ad77beb9fa21eebd7d958be01a6e09
Susanne Lundkvist <susanne.r.lundkvist@gmail.com>
2018-11-26 13:09:09 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
-  resources :students, only: :index
+  resources :students, only: [:index, :new, :create]
   get '/students/:id', to: 'students#show', as: 'student'
+
+  def index
+    Student.all
+  end
+
+  def new
+    Student.new(students: params[:first_name][:last_name])
+  end
+
+  def show
+  end
+
+
 end
